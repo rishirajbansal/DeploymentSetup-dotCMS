72f8c65d6c0f50b0309c50cd50cab255f7c0c7d4
rishirajbansal <rishiraj.architect@gmail.com>
2020-04-28 08:17:11 +0530

    Updated

--- ci-cd/pipelines/prod/build/build-handler.sh
+++ ci-cd/pipelines/prod/build/build-handler.sh
@@ -8,14 +8,14 @@ set -x #echo on
 # 4-> Create and Upload - dotCMS App Bundle & EB Deployment Bundle to S3 bucket
 cd ${APP_DIR}/aws/prod/bin/ebAutomations
 
-aws --version
+${AWS_CLI_EXEC_PATH}/aws --version
 
 echo "Calling 'createUploadAppBundle' to create and upload app bundle to S3"
 
-./createUploadAppBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP}
+./createUploadAppBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP} ${AWS_CLI_EXEC_PATH}
 
 echo "Calling 'createUploadEBDeployBundle' to create and upload EB Deployment Bundle to S3"
 
-./createUploadEBDeployBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP}
+./createUploadEBDeployBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP} ${AWS_CLI_EXEC_PATH}
 
 echo "Creating and Uploading of App Bundle and EB Deployment is done."
