124e5bdc7c7231f71ec38cd0c58fabaa02bb5d1a
rishirajbansal <rishiraj.architect@gmail.com>
2020-04-28 07:26:07 +0530

    Update build-handler.sh

--- ci-cd/pipelines/prod/build/build-handler.sh
+++ ci-cd/pipelines/prod/build/build-handler.sh
@@ -71,10 +71,10 @@ cd ${APP_DIR}/aws/prod/bin/ebAutomations
 
 echo "Calling 'createUploadAppBundle' to create and upload app bundle to S3"
 
-createUploadAppBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP}
+./createUploadAppBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP}
 
 echo "Calling 'createUploadEBDeployBundle' to create and upload EB Deployment Bundle to S3"
 
-createUploadEBDeployBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP}
+./createUploadEBDeployBundle.sh ${AWS_PROFILE_NAME} ${S3_BUCKET_NAME_APP}
 
 echo "Creating and Uploading of App Bundle and EB Deployment is done."
